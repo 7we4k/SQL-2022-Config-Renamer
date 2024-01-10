d9d5dca1290b4374e79a69bd445781c54b2bc723
7we4k <136004056+7we4k@users.noreply.github.com>
2024-01-10 12:52:43 -0600

    Updated Code

--- SQL2022AUTOINSTALLER.bat
+++ SQL2022AUTOINSTALLER.bat
@@ -6,13 +6,10 @@ ECHO Click Install when prompted
 cd\
 cd "c:Path\ToInstaller\SQL Runtime 2022 (64bit)"
 SSMS-Setup-ENU.exe
-REM This shuts off the SQL server service on workstations
-Set Server=A
 :ASK
 ECHO
 sc config SQLAgent$DBINSTANCENAMEGOESHERE start= disabled
 sc config MSSQL$DBINSTANCENAMEGOESHERE start= disabled
-:YES
 ECHO This installer has completed
 pause
 exit
