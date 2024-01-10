71d27fc32c71d6617588968c945fd008b586f046
7we4k <136004056+7we4k@users.noreply.github.com>
2024-01-10 12:17:27 -0600

    Uploaded Files
    Added all 3 files needed.

---
+++ SQL2022AUTOINSTALLER.bat
@@ -0,0 +1,27 @@
+REM SQL Runtime Auto Installer
+echo off
+ECHO Installing SQL 2022 Runtime, Instance name DBINSTANCENAMEGOESHERE
+"C:\Path\To\Installer\SQL Runtime 2022 (64bit)\setup.exe" /ConfigurationFile="c:\path\to\installer\SQL Runtime 2022 (64bit)\configurationFile.ini"
+ECHO Click in Install when prompted
+cd\
+cd "c:\impromed\SQL Runtime 2022 (64bit)"
+SSMS-Setup-ENU.exe
+REM This shuts off the SQL server service on workstations
+Set Server=A
+:ASK
+ECHO
+ECHO Is This A Server (Y/N)? :
+set /p Server= %=%
+If %Server% == y Goto YES
+If %Server% == Y Goto YES
+If %Server% == n Goto NO
+If %Server% == N Goto NO
+ECHO Not A Valid Entry
+Goto ASK
+:NO
+sc config SQLAgent$DBINSTANCENAMEGOESHERE start= disabled
+sc config MSSQL$DBINSTANCENAMEGOESHERE start= disabled
+:YES
+ECHO This installer has completed
+pause
+exit
